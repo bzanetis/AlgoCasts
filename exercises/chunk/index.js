939d60795e244c12f37f03b1121fa08f003465fd
bzanetis <britt.zanetis19@gmail.com>
2019-05-03 15:00:32 -0500

    Algo prac

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size)); //taking a properly sized chunk out of original array by using slice
+    index += size; //moving on to next chunk of elems in original array
+  }
+  return chunked;
+}
+
+// function chunk(array, size) {
+//     const chunked = [];
+//     for (let element of array) {
+//       const last = chunked[chunked.length - 1]; //get last elem of new chunked array;
+
+//       if (!last || last.length === size) {
+//         chunked.push([element]); //combining steps of creating new chunk within chunked & adding curr elem that we're iterating over to it
+//       } else {
+//         last.push(element); //else add the current elem into the chunk
+//       }
+//     }
+//     return chunked;
+//   }
 
 module.exports = chunk;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let greatest = "";
+  let max = 0;
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1; //alt clean way using ternary instead of ifs
+    //     if (charMap[char]) {
+    //       charMap[char]++;
+    //     } else {
+    //       charMap[char] = 1;
+    //     }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      greatest = char;
+    }
+  }
+  return greatest;
+}
 
 module.exports = maxChar;
