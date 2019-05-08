91fb4652c7199ede841d5092e30acb77d8f9df60
bzanetis <britt.zanetis19@gmail.com>
2019-05-08 13:48:14 -0500

    updated solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
+//helper func cleans spaces & punctuation, lowercases, splits into arr, sorts, joins back into str
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     //build logic that removes white space, nonletters & lowercase into for loop
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//     // charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
+
+//make helper function to build char maps out of string inputs
+//const obj = {a: 1, b: 2}
+//Object.keys(obj) // ["a", "b"]
 
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+//create 'result' which is the first char of the input str capitalized - means will have to use a for loop this time bc need to start after first char
+//for each char in the string
+////IF the char to the left is a space,
+//////capitalize it & add to result
+////ELSE
+//////add to result
+
+// function capitalize(str) {
+//     const words = [];
+
+//     for (let word of str.split(" ")) {
+//       words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return words.join(" ");
+//   }
+//make an empty array "words"
+////make input str an arr using split
+////for each word in the arr
+////uppercase 1st letter of the word
+////join first letter w/rest of the string
+////push result into words arr
+//join "words" into a string and return it
 
 module.exports = capitalize;
