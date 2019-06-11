02cb544e7a2d386f87449e68bded55280d71030f
bzanetis <britt.zanetis19@gmail.com>
2019-06-11 16:44:36 -0500

    algos

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,51 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+  // remember -> max length we can have in a col is determined by 2 * n -1
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+    //work in this section is done, so return (but not actually returning anything)
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  pyramid(n, row, level + add);
+}
+
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//     for (let r = 0; r < n; r++) {
+//       let level = "";
+//       for (let column = 0; column < 2 * n - 1; column++) {
+//         // get midpoint index of column --> rows will tell us how many #s should be on each side of midpoint
+//         // below checks if columns are in between a bracket of acceptable nums
+//         if (midpoint - r <= column && midpoint + r >= column) {
+//           level += "#";
+//         } else {
+//           level += " ";
+//         }
+//       }
+//       console.log(level);
+//     }
+//   }
+
+// from 0 to n (iterate through rows)
+/// create an empty string, 'level'
+/// from 0 to n * 2 - 1 (columns)
+//// if the column should have a "#"
+///// add a '#' to level
+//else
+///// add a space to level
+//console.log(level)
 
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,53 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// added in arg of row, defaulted to val of 0 in order to start on top row
+// stair also needs to be shared between func calls, so adding that w/initial default val of empty
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    //work in this section is done, so return (but not actually returning anything)
+    return;
+  }
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  // not changing the val of row here bc we're still working on same row)
+  steps(n, row, stair);
+}
+// RECURSIVE solution
+// if row === n, then we've hit our base case
+// if the stair string has a length === row, then we're at end of row (increment row at end of row)
+// if the length of stair str is <= row number we're working on, we add a '#', otherwise add a ' '
+
+// function steps(n) {
+//     for (let r = 0; r < n; r++) {
+//       let stair = "";
+
+//       for (let c = 0; c < n; c++) {
+//         if (c <= r) {
+//           stair += "#";
+//         } else {
+//           stair += " ";
+//         }
+//       }
+//       console.log(stair);
+//     }
+//   }
+
+// from 0 to n (iterate through rows)
+//// create an empty string stair "" (for each row, so, within row loop)
+//// from 0 to n (iterate thru columns)
+///// if the current column is =< the current row
+/////// add a '#' to stair
+//else
+////// add a space to stair
+// console.log stair
 
 module.exports = steps;
